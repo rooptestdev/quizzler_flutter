560a3faa15c3e482a88f659a6ca7f12d0107dbc4
rooptestdev <92123921+rooptestdev@users.noreply.github.com>
2021-12-28 06:19:04 +0530

    Initial UI fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const QuizApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey,
         body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
             child: QuizPage(),
           ),
         ),
@@ -34,6 +34,81 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                'The question will be shows here.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.green[900],
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                    )),
+                onPressed: () {
+                  print('true');
+                  //TODO: Add counting for true
+                },
+                child: const Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 10.0,
+            ),
+            Expanded(
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red[900],
+                ),
+                onPressed: () {
+                  print('false');
+                  //TODO: Add counting for false.
+                },
+                child: const Text(
+                  'False',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Row(
+          children: const [
+            Expanded(
+              child: Text('Series of icons to track the progress'),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
